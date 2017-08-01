2443d947719ef44b8ea99bf3ab01123a1534209a
Jonathan Norstrom <jon.norstrom@gmail.com>
2017-08-01 19:20:43 -0400

    strip it down to nothing

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import List from './List';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="jumbotron" style={{backgroundColor: "rgb(100, 100, 150)", color: "white"}}>
+        <div className="jumbotron">
           <h1>
-            React API Handling.
+            React API Handling
           </h1>
           <p>
             A quick rundown on API handling in React.
           </p>
         </div>
-        <List />
       </div>
     );
   }
